481a4fdd939b6dfd2e69808297e36a729a36170e
Zandarm <zandarmerija99@gmail.com>
2023-03-02 12:28:09 +0100

    querySelector

--- script.js
+++ script.js
@@ -10,11 +10,11 @@ console.log(document.all)
 console.dir(document.all(8))
 document.all(8).textContent = "Nesto drugo"
 console.log(document.getElementById("main-title"));
-
+//get item by id
 document.getElementById("main-title").textContent="Drugi naslov"
 let mainTitle=document.getElementById("main-title");
 mainTitle.textContent="Treci naslov"
-
+//get element by class
 console.log(document.getElementsByClassName("list-item"))
 let items=document.getElementsByClassName("list-item")
 console.log(items)
@@ -25,3 +25,16 @@ for (let i = 0; i< items.length; i++) {
         items[i].style.backgroundColor="grey"
     }
 }
+//get Elements by TagName
+console.log(document.getElementsByTagName("h2"))
+let naslovi=document.getElementsByTagName("h2");
+naslovi[1].style.color="red"
+
+//querySelektor
+console.log(document.querySelector("#prvo p"));
+let prviParagraf = document.querySelector("#prvo p");
+prviParagraf.style.color = "brown"
+console.log(document.querySelector("list-item"))
+console.log(document.querySelectorAll("list-item"));
+let lista = document.querySelector("list-item")
+lista[2].style.backgroundColor = "red"
